7cb2dcc3dc02c9279124e473664ff8a174e91d0a
Rahul Gupta <iamrahulgupta4u@gmail.com>
2023-11-27 13:26:48 +0530

    MongoDb connection setup

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import { mongoose } from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+export const connectDb = async () => {
+  try {
+    const instanceCreate = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log`MongoDB Instance is connected to ${instanceCreate.connection.host}`;
+  } catch (error) {
+    console.error("Error Ocurred in connecting to databases", error);
+    process.exit(1);
+  }
+};
